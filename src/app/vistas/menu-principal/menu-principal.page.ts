dcf557c5f65c73f2e76ddc8fc5639a0678002abb
Javiera <69701833+javiesp@users.noreply.github.com>
2023-11-01 20:07:07 -0300

    cambios menu principal

--- src/app/vistas/menu-principal/menu-principal.page.ts
+++ src/app/vistas/menu-principal/menu-principal.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+
 
 
 @Component({
@@ -9,25 +13,40 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['./menu-principal.page.scss'],
 })
 export class MenuPrincipalPage implements OnInit {
+  restaurantes: any[];
+  menuRestaurante: any[];
+  restauranteSeleccionado: any;
 
-  constructor(private router: Router, private loadingController: LoadingController) { }
+  constructor(private router: Router, private loadingController: LoadingController,private db: AngularFireDatabase, private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
+
+
+  //Funcion para mostrar los tipos de restarantes
+  getMenuRestaurante(restauranteId: string) {
+    const restaurante = this.restaurantes.find((rest) => rest.id === restauranteId);
+    if (restaurante) {
+      this.restauranteSeleccionado = restaurante.nombre;
+      this.menuRestaurante = restaurante.menu;
+    }
+  }
+
   async ionViewWillEnter() {
     const loading = await this.loadingController.create({
-      message: 'Cargando elementos...', // Mensaje que se mostrará
-      duration: 2000 // Duración en milisegundos (ajusta según tus necesidades)
+      message: 'Cargando, espere un momento...', // Mensaje que se mostrará
+      duration: 1000 // Duración en milisegundos (ajusta según tus necesidades)
     });
     
     await loading.present();
     
-    // Simula una carga ficticia (reemplaza esto con tu lógica de carga real)
-    setTimeout(() => {
-      loading.dismiss();
-    }, 1000); // Ajusta el tiempo según tus necesidades
+    //Carga los elementos a listar
+    this.db.list('restaurantes').valueChanges().subscribe((data: any[]) => { //Busca la data y las guarda en this.restaurantes
+      this.restaurantes = data;
+    });
   }
+
   
 
 }
