b3fa8523ccdae0fff770fe07cc31fd32dc1d6c40
Javiera <69701833+javiesp@users.noreply.github.com>
2023-12-04 01:20:34 -0300

    validación saldo compra
    instalar:
    npm i notiflix

--- src/app/vistas/pedidos/pedidos.page.ts
+++ src/app/vistas/pedidos/pedidos.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as Notiflix from 'notiflix';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pedidos',
@@ -11,8 +13,11 @@ export class PedidosPage implements OnInit {
   uid: string;
   pedidos: any[] = [];
   codigo: string;
+  saldoTotalCliente : number = 42000;
+  saldoFinal : number = 0;
+  compraValida : boolean; 
 
-  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase, private router : Router) {
     this.afAuth.authState.subscribe((user) => {
       if (user) {
         this.uid = user.uid;
@@ -46,33 +51,60 @@ export class PedidosPage implements OnInit {
       }
     });
   }
-  
-  
-
 
   //Elimina pedido de carrito de compra
   eliminarPedidosDelUsuario(usuarioID: string) {
     this.db.object(`CarritoPedidos/${usuarioID}`).remove()
       .then(() => {
         console.log('Pedidos del usuario eliminados exitosamente.');
-        this.pedidos = []; // Limpiar la lista después de eliminar
+        Notiflix.Notify.success('Pedidos del usuario eliminados exitosamente.');
+
+        this.pedidos = [];
+        this.saldoFinal = 0;
       })
       .catch((error) => {
         console.error('Error al eliminar los pedidos del usuario:', error);
       });
   }
 
-  //Calcula el total de la compra sumando los precios del carrito 
+  //Calcula el total de la compra sumando los precios del carrito y valida si no supera el saldo total
   calcularTotal(): number {
     let total = 0;
+  
     if (this.pedidos && this.pedidos.length > 0) {
       this.pedidos.forEach((pedido: any) => {
-        total += pedido?.Pedido?.precio || 0; // Suma el precio del pedido al total
-
+        total += pedido?.Pedido?.precio || 0; 
+        if (total >= this.saldoTotalCliente) {
+          total = -1;
+          if (total = -1) {
+            this.saldoFinal = 0;
+            Notiflix.Notify.failure('El total de la compra supera el saldo');
+          }
+          this.compraValida = false;
+          return total; 
+        } else {
+          this.saldoFinal = this.saldoTotalCliente - total;
+          this.compraValida = true;
+          return total
+        }
       });
+
+
     }
+  
     return total;
   }
+
+  continuarCompra(){
+    if(this.compraValida = true){
+      this.router.navigate(['/home']);
+      Notiflix.Notify.success('Continuar con la compra');
+    }else{
+      Notiflix.Notify.failure('El total de la compra supera el saldo');
+    }
+  }
+  
+
 
 
   
